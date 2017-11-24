380c505eb8260c8f287638400a006c1a47d51d40
dzara004 <dzara004@ucr.edu>
2017-11-24 14:34:42 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,84 @@
+/////////////////////////////////
+/*CODE FOR 80-90 POINT PROJECT */
+/////////////////////////////////
+
+#include <avr/io.h>
+#include "usart_ATmega1284.h"
+#include "timer.h"
+
+/* GLOBAL VARIABLES */
+unsigned char run;			//Determines whether to turn on the pump or not
+
+enum States{wait, pump} state;
+
+void Tick() {
+	switch(state) {
+		case wait:
+			if (run == 0) {
+				state = wait;
+			} else if (run == 1) {
+				state = pump;
+				PORTC = 0x01;
+			} else {
+				state = wait;
+			}
+			break;
+		case pump:
+			if (run == 1) {
+				state = pump;
+			} else if (run == 0) {
+				state = wait;
+				PORTC = 0x00;
+			} else {
+				state = wait;
+			}
+			break;
+		default:
+			break;
+	}
+	
+	switch(state) {
+		case wait:
+			if (USART_HasReceived(1)) {
+				run = USART_Receive(1);
+				USART_Flush(1);
+			}
+			break;
+		case pump:
+			if (USART_HasReceived(1)) {
+				run = USART_Receive(1);
+				USART_Flush(1);
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+int main(void)
+{
+	//Uses second ATmega1284
+	DDRA = 0x00; PORTA = 0x00;	//Buttons for testing (Placeholders for water sensor)
+	DDRC = 0xFF; PORTC = 0x00;  //For DC Motor and white OLED
+	DDRD = 0x0A; PORTD = 0x00;	//For USART
+	
+	//Initializations
+	initUSART(1);
+	run = 0;
+	state = wait;
+	
+	TimerSet(200);
+	TimerOn();
+	
+	while (1)
+	{
+		if (USART_IsSendReady(1)) {
+			USART_Send(PINA, 1);
+			while (!USART_HasTransmitted(1)) {}
+		}
+		Tick();
+		while (!TimerFlag) {}
+		TimerFlag = 0;
+	}
+}
+
