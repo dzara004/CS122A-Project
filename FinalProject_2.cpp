eefcdf2e4cce8cff2e8196bb3a81dd572f1674b3
dzara004 <dzara004@ucr.edu>
2017-12-05 11:04:31 -0800

    Fixed formalities in comments

--- FinalProject_2.cpp
+++ FinalProject_2.cpp
@@ -14,8 +14,8 @@
 
 /* GLOBAL VARIABLES */
 unsigned char run;			//Determines whether to turn on the pump or not
-unsigned char level;		//Holds the level of the water
-unsigned char phone;		//Returns the input of the phone
+unsigned char level;			//Holds the level of the water
+unsigned char phone;			//Returns the input of the phone
 Framebuffer fb;				//Variable for writing to OLED
 unsigned char i;			//Index for looping
 
@@ -45,38 +45,25 @@ unsigned char cups1[32] = {'B', 'e', 't', 'w', 'e', 'e', 'n', ' ', '0', '.', '7'
 unsigned char cups2[32] = {'B', 'e', 't', 'w', 'e', 'e', 'n', ' ', '3', '.', '8', '0', ' ', 'C', 'u', 'p', 's', ' ', 'a', 'n', 'd', ' ', '7', '.', '8', '2', ' ', 'C', 'u', 'p', 's', '\n'};
 unsigned char cups3[16] = {'A', 'b', 'o', 'v', 'e', ' ', '7', '.', '8', '2', ' ', 'C', 'u', 'p', 's', '\n'};
 
-// 0.954 hz is lowest frequency possible with this function,
-// based on settings in PWM_on()
-// Passing in 0 as the frequency will stop the speaker from generating sound
+/* PWM FUNCTIONS (Taken from CS120B) */
 void set_PWM(double frequency) {
-	static double current_frequency; // Keeps track of the currently set frequency
-	// Will only update the registers when the frequency changes, otherwise allows
-	// music to play uninterrupted.
+	static double current_frequency; 
 	if (frequency != current_frequency) {
-		if (!frequency) { TCCR0B &= 0x08; } //stops timer/counter
-		else { TCCR0B |= 0x03; } // resumes/continues timer/counter
+		if (!frequency) { TCCR0B &= 0x08; }
+		else { TCCR0B |= 0x03; } 
 		
-		// prevents OCR3A from overflowing, using prescaler 64
-		// 0.954 is smallest frequency that will not result in overflow
-		if (frequency < 0.954) { OCR0A = 0xFFFF; }
-		
-		// prevents OCR0A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
+		if (frequency < 0.954) { OCR0A = 0xFFFF; }				
 		else if (frequency > 31250) { OCR0A = 0x0000; }
-		
-		// set OCR3A based on desired frequency
 		else { OCR0A = (short)(8000000 / (128 * frequency)) - 1; }
 
-		TCNT0 = 0; // resets counter
-		current_frequency = frequency; // Updates the current frequency
+		TCNT0 = 0; 
+		current_frequency = frequency; 
 	}
 }
 
 void PWM_on() {
 	TCCR0A = (1 << COM0A0 | 1 << WGM00);
-	// COM3A0: Toggle PB3 on compare match between counter and OCR0A
 	TCCR0B = (1 << WGM02) | (1 << CS01) | (1 << CS00);
-	// WGM02: When counter (TCNT0) matches OCR0A, reset counter
-	// CS01 & CS30: Set a prescaler of 64
 	set_PWM(0);
 }
 
@@ -85,6 +72,7 @@ void PWM_off() {
 	TCCR0B = 0x00;
 }
 
+/* OLED FUNCTIONS */
 void PumpOff(Framebuffer);
 void PumpOn(Framebuffer);
 
@@ -335,9 +323,9 @@ void pumpTick() {
 int main(void)
 {
 	//Uses second ATmega1284
-	DDRA = 0x00; PORTA = 0x00;	//Buttons for testing (Placeholders for water sensor)
+	DDRA = 0x00; PORTA = 0x00;	//Water sensor input
 	DDRB = 0xFF; PORTB = 0x00;	//For the speaker
-	DDRC = 0xFF; PORTC = 0x00;  //For DC Motor and white OLED
+	DDRC = 0xFF; PORTC = 0x00;  	//For DC Motor and white OLED
 	DDRD = 0x0A; PORTD = 0x00;	//For USART
 	
 	//Initializations
